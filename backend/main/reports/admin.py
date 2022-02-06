f688e1322a13c3a442e1e062d3d77ec8868b30a3
dxghost <34963143+dxghost@users.noreply.github.com>
2022-02-06 22:10:56 +0330

    Feature/3 minimal reporter backend (#4)
    * Initialize django project
    
    * Fix requirements file, wrong environment was committed
    
    * Ignore .vscode dir entirely
    
    * Add reports app with a minimal list view
    
    * Add swagger to the project root url
    
    * Add corsheaders middleware
    
    * Add postgres as database backend
    
    Co-authored-by: dxghost <dxghost@github.com>

---
+++ backend/main/reports/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from reports.models import IncidentReport
+
+admin.site.register(IncidentReport)
---
+++ backend/main/reports/urls.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+from reports.views import IncidentReportViewset
+
+
+router = routers.SimpleRouter()
+router.register("", IncidentReportViewset)
