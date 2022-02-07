c4a484870f4360561307e4bdc4ddd9f0a1060da5
dxghost <dxghost@github.com>
2022-02-07 23:39:00 +0330

    Add simple logs view with hardcoded backend url

--- frontend/matrix-logs/src/App.vue
+++ frontend/matrix-logs/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Logs</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <p> {{ post.message }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Matrix",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get("http://localhost:8000/reports/");
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: "Spotnik";
+  src: local("Spotnik"), url(./fonts/Spotnikdemo-K7dpy.ttf) format("truetype");
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: monospace;
+  font-size: 2vh;
+  color: #19ff71;
+}
+html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  background-color: black;
+}
+h1{
+  font-size:7vh;
+  font-family:Spotnik;
+  color: gold;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin:0;
+  padding:0;
+}
+p{
+ margin: 0;
+ padding: 0;
 }
 </style>
