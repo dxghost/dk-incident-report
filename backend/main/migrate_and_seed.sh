2e4708b9787f314ab3196d5fa8196abaab6f611a
dxghost <dxghost@github.com>
2022-02-10 01:45:47 +0330

    Seed only the first time

--- backend/main/migrate_and_seed.sh
+++ backend/main/migrate_and_seed.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
 
-python manage.py migrate
-python manage.py seed reports --number 15
+pendings=`python3 manage.py showmigrations | grep '\[ \]' | wc -l`
+if [ $pendings -ge 1 ]
+then
+    python3 manage.py migrate
+    python3 manage.py seed reports --number 15
+fi
