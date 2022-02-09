002bd118493b89c63a20c301e30931e229a2ca5f
dxghost <dxghost@github.com>
2022-02-10 02:15:58 +0330

    Create superuser one migrations are applied

--- backend/monitoring/migrate_and_seed.sh
+++ backend/monitoring/migrate_and_seed.sh
@@ -4,5 +4,6 @@ pendings=`python3 manage.py showmigrations | grep '\[ \]' | wc -l`
 if [ $pendings -ge 1 ]
 then
     python3 manage.py migrate
+    python3 manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --noinput
     python3 manage.py seed logs --number 400
 fi
